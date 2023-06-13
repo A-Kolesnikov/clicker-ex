9947a5caa061b68db8352f04f8152e554761b82f
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-06-13 18:46:32 +0300

    added content for components to be interactive

--- src/project-clicker/Clicker.js
+++ src/project-clicker/Clicker.js
@@ -13,19 +13,21 @@ export class Clicker extends Component {
         this.getNumber = this.getNumber.bind(this);
 
     }
-    getNumber(number) {
-        console.log('num')
+    getNumber(text) {
+        this.setState({number: text})
     }
 
     render() {
         const segment = < Segment num1 = {1} num2 = {2} />
         return (
             <div className="container border">
-            
-            < Segment num1 = {1} num2 = {2} />
-            < Segment num1 = {3} num2 = {4} />
+            < Segment num1 = {1} num2 = {2} kbGetNumber = {this.getNumber}/>
+            < Segment num1 = {2} num2 = {4} kbGetNumber = {this.getNumber}/>
+            < Segment num1 = {3} num2 = {6} kbGetNumber = {this.getNumber}/>
+            < Segment num1 = {4} num2 = {8} kbGetNumber = {this.getNumber}/>
+            < Segment num1 = {5} num2 = {10} kbGetNumber = {this.getNumber}/>
+            < Segment num1 = {6} num2 = {12} kbGetNumber = {this.getNumber}/>
             < Answer num = {this.state.number} />
-
             </div>
         )
     }
