16c2c98e0d5058e19512c3fc736636f1341a2a45
A-Kolesnikov <kolesnikov.spb@gmail.com>
2023-06-13 20:31:37 +0300

    added styling

--- src/project-clicker/Clicker.js
+++ src/project-clicker/Clicker.js
@@ -8,26 +8,27 @@ export class Clicker extends Component {
     constructor() {
         super();
         this.state = {
-            number: '99',
+            number: 'Nothing',
         };
         this.getNumber = this.getNumber.bind(this);
 
     }
     getNumber(text) {
-        this.setState({number: text})
+        this.setState({ number: text })
     }
 
     render() {
-        const segment = < Segment num1 = {1} num2 = {2} />
         return (
-            <div className="container border">
-            < Segment num1 = {1} num2 = {2} kbGetNumber = {this.getNumber}/>
-            < Segment num1 = {2} num2 = {4} kbGetNumber = {this.getNumber}/>
-            < Segment num1 = {3} num2 = {6} kbGetNumber = {this.getNumber}/>
-            < Segment num1 = {4} num2 = {8} kbGetNumber = {this.getNumber}/>
-            < Segment num1 = {5} num2 = {10} kbGetNumber = {this.getNumber}/>
-            < Segment num1 = {6} num2 = {12} kbGetNumber = {this.getNumber}/>
-            < Answer num = {this.state.number} />
+            <div className={'container flex-column justify-content-center'}>
+                
+                    < Segment num1={1} num2={2} kbGetNumber={this.getNumber} />
+                    < Segment num1={2} num2={4} kbGetNumber={this.getNumber} />
+                    < Segment num1={3} num2={6} kbGetNumber={this.getNumber} />
+                    < Segment num1={4} num2={8} kbGetNumber={this.getNumber} />
+                    < Segment num1={5} num2={10} kbGetNumber={this.getNumber} />
+                    < Segment num1={6} num2={12} kbGetNumber={this.getNumber} />
+                    < Answer num={this.state.number} />
+                
             </div>
         )
     }
--- src/project-clicker/Segment.js
+++ src/project-clicker/Segment.js
@@ -5,16 +5,13 @@ import { useState } from 'react';
 export class Segment extends Component {
     constructor() {
         super();
-        this.state = {
-            //counter: 0,
-        };
     }
 
     render() {
         return (
-            <div className="row">
-                    <button className='col-1' onClick={(e) => this.props.kbGetNumber(e.target.textContent)}>{this.props.num1}</button>
-                    <button className='col-1' onClick={(e) => this.props.kbGetNumber(e.target.textContent)}>{this.props.num2}</button>
+            <div className={"row justify-content-center"}>
+                    <button className={`btn btn-outline-danger col-3`} onClick={(e) => this.props.kbGetNumber(e.target.textContent)}>{this.props.num1}</button>
+                    <button className={"btn btn-outline-success col-3"} onClick={(e) => this.props.kbGetNumber(e.target.textContent)}>{this.props.num2}</button>
             </div>
         )
     }
